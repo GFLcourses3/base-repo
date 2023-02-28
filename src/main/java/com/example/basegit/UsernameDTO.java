c6232426ba0cebb5dd5bf832ac697bec28e14642
Artem <93591925+artemkedyk@users.noreply.github.com>
2023-02-28 10:39:51 +0200

    - Create first test. (#1)

---
+++ src/main/java/com/example/basegit/UsernameDTO.java
@@ -0,0 +1,45 @@
+package com.example.basegit;
+
+import java.util.Objects;
+
+public class UsernameDTO {
+    private Integer id;
+    private String type;
+
+    public UsernameDTO() {
+    }
+
+    public UsernameDTO(Integer id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UsernameDTO)) return false;
+        UsernameDTO that = (UsernameDTO) o;
+        return Objects.equals(getId(), that.getId()) && Objects.equals(getType(), that.getType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getType());
+    }
+}
